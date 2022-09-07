3b3ebd3abe9a49e6716f685bde1d349fb64a4f6a
0x5459 <0x5459@protonmail.com>
2022-09-07 10:45:46 +0800

    add bufreader and bufwriter feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,22 @@
 #![feature(allocator_api)]
 #![feature(vec_into_raw_parts)]
 
+#[cfg(feature = "bufreader")]
 mod bufreader;
+#[cfg(feature = "bufwriter")]
 mod bufwriter;
+
+#[cfg(feature = "bufreader")]
+pub use bufreader::BufReader;
+#[cfg(feature = "bufwriter")]
+pub use bufwriter::BufWriter;
 
 use std::mem;
 
 #[repr(C, align(4096))]
-struct Aligned4096([u8; 4096]);
+pub struct Aligned4096([u8; 4096]);
 
-unsafe fn aligned_vec<Align>(n_bytes: usize) -> Vec<u8> {
+pub unsafe fn aligned_vec<Align>(n_bytes: usize) -> Vec<u8> {
     // Lazy math to ensure we always have enough.
     let n_units = (n_bytes / mem::size_of::<Align>()) + 1;
 
